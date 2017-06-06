70f93ba2cd6c03c14407df12e4e95867ed98c5ba
D4rkS1De <u4eba.ucheba2016@yandex.ru>
2017-06-07 00:22:50 +0700

    Update login.php

--- login.php
+++ login.php
@@ -8,22 +8,27 @@
                   <h2 class="post_ttl2">АВТОРИЗАЦИЯ</h2>
                   <div id="basket" style="background:none;">
                      <?php
-			$login     = htmlspecialchars($_POST['login']);
-			$password  = htmlspecialchars($_POST['password']);
-			$logins    = file('files/logins_j2Ik9JsqU2Y82hd7C4bD7.txt', FILE_IGNORE_NEW_LINES);
-			$passwords = file('files/passwords_l0Kj7dYqhs8YTm4N.txt', FILE_IGNORE_NEW_LINES);
-			for ($i = 0; $i < count($logins); $i++) {
-			    if ($login == $logins[$i] && $password == $passwords[$i]) {
-				$_SESSION['login'] = $login;
-				if ($_SESSION['login'] == 'sanya007' && $password == 'password') {
-				    $_SESSION['adminmode'] = 'activated';
-				}
-				echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
-				exit;
-			    }
+			if (isset($_SESSION['login'])) {
+				echo '<script>location.href="index.php"</script>';
 			}
-			echo '<p align="center" style="font-size:28px;color:#DFAA1E;"><b>Ошибка ввода, Попробуйте снова</b></p>';
-			?>
+			else {  
+				$login     = htmlspecialchars($_POST['login']);
+				$password  = htmlspecialchars($_POST['password']);
+				$logins    = file('files/logins_j2Ik9JsqU2Y82hd7C4bD7.txt', FILE_IGNORE_NEW_LINES);
+				$passwords = file('files/passwords_l0Kj7dYqhs8YTm4N.txt', FILE_IGNORE_NEW_LINES);
+				for ($i = 0; $i < count($logins); $i++) {
+				    if ($login == $logins[$i] && $password == $passwords[$i]) {
+					$_SESSION['login'] = $login;
+					if ($_SESSION['login'] == 'sanya007' && $password == 'password') {
+					    $_SESSION['adminmode'] = 'activated';
+					}
+					echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
+					exit;
+				    }
+				}
+				echo '<p align="center" style="font-size:28px;color:#DFAA1E;"><b>Ошибка ввода, Попробуйте снова</b></p>';
+			}
+		 ?>
                   </div>
                </div>
             </div>
